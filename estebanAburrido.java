a7afedaebad528668345be7c3ef72a597b284473
Esteban Lobos <esteban.hernan.lobos@gmail.com>
2022-08-17 09:40:30 -0400

    un class de java que dice hola y lo vigilo mas que eso no mucho

---
+++ estebanAburrido.java
@@ -0,0 +1,7 @@
+package appmviles;
+class Hola{
+    public static void main(String[] args) {
+        System.out.println("Hola profe julio :)");
+        System.out.println("lo vigilo -_-");
+    }
+}
